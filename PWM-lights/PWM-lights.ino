3dfbfbd8ea7a52a660456303d921ce4414109006
mam249 <60033552+mam249@users.noreply.github.com>
2021-06-02 23:09:23 +0100

    Update PWM-lights.ino

--- PWM-lights/PWM-lights.ino
+++ PWM-lights/PWM-lights.ino
@@ -3,9 +3,14 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 int greenPin = 3;
 int bluePin = 2;
+int x;
+int y;
+int noOfHits = 0;
+int drumPadList[] = {2,3,4,5,6,7,8,9};
 const int DRUM_PAD = 3;
 unsigned long time_now = 0;
-
+int bluePinList[] = {0,2,4,6,8,10,12};
+int greenPinList[] = {1,3,5,7,9,11,13};
 void setup() {
   pwm.begin();
   pwm.setPWMFreq(1600);  // This is the maximum PWM frequency
@@ -14,39 +19,39 @@ void setup() {
   pwm.setPWM(greenPin, 4096, 0);
   pwm.setPWM(0, 4096, 0);
   pwm.setPWM(1, 4096, 0);
-  pwm.setPWM(3, 4096, 0);
   pwm.setPWM(4, 4096, 0);
   pwm.setPWM(5, 4096, 0);
   pwm.setPWM(6, 4096, 0);
   pwm.setPWM(7, 4096, 0);
   pwm.setPWM(8, 4096, 0);
-  pwm.setPWM(0, 4096, 0);
   pwm.setPWM(9, 4096, 0);
   pwm.setPWM(10, 4096, 0);
   pwm.setPWM(11, 4096, 0);
   pwm.setPWM(12, 4096, 0);
   pwm.setPWM(13, 4096, 0);
-
+  x=0;
+  y=0;
 
 
 }
 
 void loop() {
-  bluePinLoop();
-  greenPinHit();
-}
-
-void greenPinHit() {
-  time_now
-  if (digitalRead(DRUM_PAD)==HIGH) {
-    
+  time_now = millis();
+  while (millis() < time_now + 900) {
+    pwm.setPWM(bluePinList[x], 0, 4096);
+    y=x;
+    if (digitalRead(drumPadList[x])==HIGH) {
+      noOfHits+=1;
+      pwm.setPWM(greenPinList[x], 0, 4096);
+      pwm.setPWM(bluePinList[x], 4096, 0);
+//      x=(x+1)%7;
+      x = random(0,7);
+      delay(400);
+      break;
+      }
     }
-  }
-
-void bluePinLoop() {
-  pwm.setPWM(bluePin, 0, 4096);
-  delay(500);
-  pwm.setPWM(bluePin, 4096, 0);
-  delay(1116);
-  
-  }
+  while (millis() < time_now + 1616) {
+    pwm.setPWM(bluePinList[y], 4096, 0);
+    pwm.setPWM(greenPinList[y], 4096, 0);
+    }
+}
